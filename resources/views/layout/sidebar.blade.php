371573b376e3fff20ef0106feeafa18c384c2213
aryafaiq <aryafaiq17@gmail.com>
2026-05-21 15:54:45 +0700

    LOGO

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PDL Invoice</title>
-  <!-- Menggunakan Bootstrap 5.3 via CDN -->
+  <link rel="icon" href="{{ asset('img/logopdl.png') }}">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   <!-- Menggunakan Lucide Icons untuk icon yang clean -->
   <script src="https://unpkg.com/lucide@latest"></script>
