cb8b4f052382983720f45ac418d943f9e18b539b
aryafaiq <aryafaiq17@gmail.com>
2026-06-03 17:29:50 +0700

    Route Create recepient

--- resources/views/perusahaan.blade.php
+++ resources/views/perusahaan.blade.php
@@ -36,51 +36,9 @@
 
 @section('content')
     <div class="container">
-        <a href="/" class="btn btn-secondary mb-2"><i data-lucide="arrow-big-left-dash"></i>Back
+        <a href="{{ route('perusahaan.create') }}" class="btn btn-secondary mb-2"><i data-lucide="id-card"></i>Add New Recipient
         </a>
         <div class="row">
-            <div class="col-md-8 col-lg-12">
-
-                <!-- CARD FORM CONTAINER -->
-                <form action="{{ route('perusahaan.store') }}" method="POST">
-                    @csrf
-                    <div class="card p-4 bg-white">
-                        <!-- Form Header -->
-                        <div class="mb-4">
-                            <h2 class="fs-4 fw-bold text-dark m-0">Recipient Data</h2>
-                            <div class="d-flex justify-content-end gap-2">
-                                <button type="submit" class="btn btn-submit">Simpan Data</button>
-                            </div>
-                        </div>
-
-                        <!-- FORM START -->
-
-                        <!-- Row 1: Dua Kolom (Nama & Email) -->
-                        <div class="row g-3 mb-3">
-                            <div class="col-sm-6">
-                                <label for="inputNama" class="form-label text-secondary fw-medium small">Nama Perusahaan</label>
-                                <input type="text" class="form-control rounded-3" id="inputNama" name="nama_perusahaan">
-                            </div>
-                            <div class="col-sm-6">
-                                <label for="" class="form-label text-secondary fw-medium small">Alamat</label>
-                                <textarea type="textarea" class="form-control rounded-3" id="inputEmail" name="alamat_perusahaan"
-                                    placeholder="" value=""></textarea>
-                            </div>
-                            <div class="col-sm-6">
-                                <label for="" class="form-label text-secondary fw-medium small">PIC</label>
-                                <input type="text" class="form-control rounded-3" id="inputEmail" name="pic"
-                                    placeholder="" value="">
-                            </div>
-                            <div class="col-sm-6">
-                                <label for="" class="form-label text-secondary fw-medium small">No Tlp</label>
-                                <input type="text" class="form-control rounded-3" id="inputEmail" name="no_tlp"
-                                    placeholder="+62" value="">
-                            </div>
-                        </div>
-                    </div>
-                </form>
-
-            </div>
             <div class="card bg-white mt-2">
                 <table class="table table-hover align-middle m-0" style="min-width: 900px;">
                 <thead class="">
